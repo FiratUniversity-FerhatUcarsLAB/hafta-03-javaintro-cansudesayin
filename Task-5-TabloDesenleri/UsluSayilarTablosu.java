8fee4abcb574b3a725579d97304f3e9427283be7
cansudesayin <250541096@firat.edu.tr>
2025-10-23 03:12:50 +0300

    Update UsluSayilarTablosu.java

---
+++ Task-5-TabloDesenleri/UsluSayilarTablosu.java
@@ -0,0 +1,22 @@
+public class UsluSayilar {
+    public static void main(String[] args) {
+        // Hedef Çıktı:
+        System.out.println("a\ta^2\ta^3");
+        System.out.println("-----------------");
+
+        int a1 = 1;
+        System.out.println(a1 + "\t" + (a1 * a1) + "\t" + (a1 * a1 * a1));
+
+        int a2 = 2;
+        System.out.println(a2 + "\t" + (a2 * a2) + "\t" + (a2 * a2 * a2));
+
+        int a3 = 3;
+        System.out.println(a3 + "\t" + (a3 * a3) + "\t" + (a3 * a3 * a3));
+
+        int a4 = 4;
+        System.out.println(a4 + "\t" + (a4 * a4) + "\t" + (a4 * a4 * a4));
+
+        int a5 = 5;
+        System.out.println(a5 + "\t" + (a5 * a5) + "\t" + (a5 * a5 * a5));
+    }
+}
