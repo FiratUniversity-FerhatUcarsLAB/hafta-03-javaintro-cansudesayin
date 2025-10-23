c8ad9b7b04c6d282e5c397c6e14ab242c8e21685
cansudesayin <250541096@firat.edu.tr>
2025-10-23 03:03:54 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,9 @@
+public class AsciiArt {
+    public static void main(String[] args) {
+        System.out.println(" CCCCCCCCCC ");
+        System.out.println("CCC      ");
+        System.out.println("CCC      ");
+        System.out.println("CCC      ");
+        System.out.println(" CCCCCCCCCC ");
+    }
+}
