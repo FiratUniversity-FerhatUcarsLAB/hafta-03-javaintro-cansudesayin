2bd83f6c6aa3bf7481264f7d9bb739f3e70a8b40
cansudesayin <250541096@firat.edu.tr>
2025-10-23 03:21:18 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,9 @@
+public class AsciiArtIsim {
+    public static void main(String[] args) {
+        System.out.println("CCCC   AAAAA  N   N  SSSS  U   U  DDDD   EEEEE");
+        System.out.println("C      A   A  NN  N  S     U   U  D   D  E");
+        System.out.println("C      AAAAA  N N N  SSS   U   U  D   D  EEEE");
+        System.out.println("C      A   A  N  NN     S  U   U  D   D  E");
+        System.out.println("CCCC   A   A  N   N  SSSS   UUU   DDDD   EEEEE");
+    }
+}
